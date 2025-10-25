4f9e2bdf488dc4316ee215c97a3e55aae9942246
juanealop <88118954+juanealop@users.noreply.github.com>
2025-10-25 16:15:12 -0500

    feat: arreglando lo de la entrega pasada

---
+++ configuracion-server/src/main/java/com/proyectoMicro/configuracion_server/ConfiguracionServerApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoMicro.configuracion_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguracionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguracionServerApplication.class, args);
+	}
+
+}
---
+++ configuracion-server/src/test/java/com/proyectoMicro/configuracion_server/ConfiguracionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoMicro.configuracion_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
